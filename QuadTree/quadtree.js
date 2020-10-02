6dab625885ab1e134b037a8e9ab6f15946957b7e
Oscar Pfuturi <opfuturi@unsa.edu.pe>
2020-10-01 21:08:15 -0500

    quadtree, draw

--- QuadTree/quadtree.js
+++ QuadTree/quadtree.js
@@ -1,5 +1,5 @@
 class Point {
-	constructor (x, y, userData ){
+	constructor (x, y, userData){
 		this.x = x;
 		this.y = y;
 		this.userData = userData ;
@@ -14,7 +14,7 @@ class Rectangle {
 		this.h = h; // half height
 	}
 	// verifica si este objeto contiene un objeto Punto
-	contains(point){
+	contains (point) {
 		if (point.x >= this.x - this.w && point.x <= this.x + this.w && 
 			point.y >= this.y - this.h && point.y <= this.y + this.h){
 			return 1;
@@ -22,62 +22,69 @@ class Rectangle {
 		return 0;
 	}
 	// verifica si este objeto se intersecta con otro objeto Rectangle
-	intersects(range){
+	intersects (range) {
 		return !(range.x - range.w > this.x + this.w || range.x + range.w < this.x - this.w ||
 				range.y - range.h > this.y + this.h || range.y + range.h < this.y - this.h);
 	}
 }
 
 class QuadTree {
-	constructor(boundary,n){
-		this.boundary = boundary ; // Rectangle
+	constructor(boundary, n) {
+		this.boundary = boundary; // Rectangle
 		this.capacity = n; // capacidad maxima de cada cuadrante
 		this.points = []; // vector , almacena los puntos a almacenar
-		this.divided = false ;
+		this.divided = false;
 	}
 	// divide el quadtree en 4 quadtrees
-	subdivide(){
-		const qt_northeast = new QuadTree;
-		const qt_northwest = new QuadTree;
-		const qt_southeast = new QuadTree;
-		const qt_southwest = new QuadTree;
-		this.northeast = qt_northeast ;
-		this.northwest = qt_northwest ;
-		this.southeast = qt_southeast ;
-		this.southwest = qt_southwest ;
+	subdivide() {
+		let x = this.boundary.x;
+		let y = this.boundary.y;
+		let w = this.boundary.w;
+		let h = this.boundary.h;
+
+		let r_northeast = new Rectangle(x + w/2, y + h/2, w/2, h/2);
+		let r_northwest = new Rectangle(x - w/2, y + h/2, w/2, h/2);
+		let r_southeast = new Rectangle(x + w/2, y - h/2, w/2, h/2);
+		let r_southwest = new Rectangle(x - w/2, y - h/2, w/2, h/2);
+		this.northeast = new QuadTree(r_northeast, this.capacity);
+		this.northwest = new QuadTree(r_northwest, this.capacity);
+		this.southeast = new QuadTree(r_southeast, this.capacity);
+		this.southwest = new QuadTree(r_southwest, this.capacity);
 
 		this.divided = true;
 	}
 
-	insert(point){
-		if (!this.boundary.contains(point)){
+	insert(point) {
+		if (!this.boundary.contains(point)) {
 			return ;
 		}
-		if (this.points.length < this.capacity){
+		if (this.points.length < this.capacity) {
 			this.points.push(point);
 		}
 		else {
-			subdivide();
-			this.northeast.push(point);
-			this.northwest.push(point);
-			this.southeast.push(point);
-			this.southwest.push(point);
+			if (!this.divided) {
+				this.subdivide();
+			}
+			this.northeast.insert(point);
+			this.northwest.insert(point);
+			this.southeast.insert(point);
+			this.southwest.insert(point);
 		}
 	}
 
-	show(){
+	show() {
 		stroke(255);
 		strokeWeight(1);
 		noFill();
 		rectMode(CENTER);
-		rect(this.boundary.x, this.boundary.y, this.boundary.w*2 , this.boundary.h *2);
-		if (this.divided){
+		rect(this.boundary.x, this.boundary.y, this.boundary.w*2 , this.boundary.h*2);
+		if (this.divided) {
 			this.northeast.show();
 			this.northwest.show();
 			this.southeast.show();
 			this.southwest.show();
 		}
-		for (let p of this.points){
+		for (let p of this.points) {
 			strokeWeight(4);
 			point(p.x, p.y);
 		}
--- QuadTree/sketch.js
+++ QuadTree/sketch.js
@@ -7,10 +7,22 @@ function setup() {
 	let boundary = new Rectangle(200, 200, 200, 200);
 	// each leave just could have 4 elements
 	qt = new QuadTree(boundary, 4);
-	console.log(qt);
-	for (let i=0; i<3; i++) {
-		let p=new Point(Math.random() * 400, Math.random() * 400) ;
+	/*console.log(qt);
+	for (let i=0; i<17; i++) {
+		let p=new Point(Math.random() * 400, Math.random() * 400);
 		qt.insert(p);
+	}
+	background(0);
+	qt.show();*/
+}
+
+function draw() {
+	background(0);
+	if (mouseIsPressed) {
+		for (let i = 0; i < 1; i++) {
+			let m = new Point(mouseX + random(-5, 5), mouseY + random(-5, 5));
+			qt.insert(m);
+		}
 	}
 	background(0);
 	qt.show();
